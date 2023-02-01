3b8f442e98ef10d82c3a84be9ce9a9694ae47b3a
Patrick Miller <paddymills@proton.me>
2023-02-01 14:36:14 -0500

    add homework2 files

---
+++ app/src/main/java/com/example/homework2/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.homework2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // set data structures spinner values
+        // adapted from the android docs: https://developer.android.com/develop/ui/views/components/spinner#java
+
+        // get spinner by id
+        Spinner ds_spinner = (Spinner) findViewById( R.id.spinnerDataStructure );
+
+        // create ArrayAdapter from string-array resources
+        ArrayAdapter<CharSequence> ds_adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.data_structures,
+                android.R.layout.simple_spinner_item
+        );
+
+        // set adapter layout
+        ds_adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
+
+        // apply adapter to spinner
+        ds_spinner.setAdapter( ds_adapter );
+
+        setContentView(R.layout.activity_main);
+    }
+}
